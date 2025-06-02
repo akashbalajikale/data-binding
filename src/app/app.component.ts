1615f0a6d03361ae3796f402f8d1999ccbfb0b7a
akash kale <kale64136@gmail.com>
2025-06-02 23:52:55 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,13 +85,13 @@ OnRemove(){
 
 
 
-  person : Array<any> =[
-    { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User' },
-    { id: 3, name: 'Alice Brown', email: 'alice@example.com', role: 'Editor' },
-    { id: 4, name: 'Bob White', email: 'bob@example.com', role: 'User' },
-    { id: 5, name: 'Charlie Green', email: 'charlie@example.com', role: 'Admin' }
-  ]
+  // person : Array<any> =[
+  //   { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
+  //   { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User' },
+  //   { id: 3, name: 'Alice Brown', email: 'alice@example.com', role: 'Editor' },
+  //   { id: 4, name: 'Bob White', email: 'bob@example.com', role: 'User' },
+  //   { id: 5, name: 'Charlie Green', email: 'charlie@example.com', role: 'Admin' }
+  // ]
  
   //  cricketers : Array<any> = [
   //   {
