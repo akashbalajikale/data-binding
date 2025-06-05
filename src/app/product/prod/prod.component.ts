75a51bd259223a12f1186fee380ad90c6d03073f
akash kale <kale64136@gmail.com>
2025-06-05 18:57:51 +0530

    work ngif directive

--- src/app/product/prod/prod.component.ts
+++ src/app/product/prod/prod.component.ts
@@ -7,19 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdComponent implements OnInit {
 
- isLoggedIn = false;
+  isLoggedIn = false;
   status = 'inactive';
   items = ['Angular', 'React', 'Vue'];
-
-
   isActive = false;
 
   toggleClass() {
     this.isActive = !this.isActive;
   }
+
+  public productName : string = "Apple 16";
+  public productId : number = 123;
+  isProductAav !: boolean;
+  color !:string;
   constructor() { }
 
   ngOnInit(): void {
+    // this.isProductAav = Math.random() >= .5 ? true : false;
+    this.setcolor()
+  }
+
+  setcolor(){
+    let flag = Math.random()
+
+    if(flag >= .7){
+      this.color = "success"
+    }else if(flag >= .5 && flag <= .7){
+      this.color = "warning"
+    }else{
+      this.color = "error"
+    }
   }
 
 }
